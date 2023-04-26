4edfff5f240bbc07a1adaad2d1c33527fa939ff3
Andrej Prskalo <aprskalo00@gmail.com>
2023-04-26 14:14:26 +0200

    improvements to react grid layout code

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -1,20 +1,20 @@
 import {ReactRouter} from '@tanstack/react-router';
 
 import {homeRoute} from '@/features/home/routes';
-import {postRoute} from '@/features/posts/views/post';
-import {postsRoute} from '@/features/posts/views/posts';
 import {indexRoute} from '@/features/posts/views/index';
-import {postsIndexRoute} from '@/features/posts/views/postsIndex';
 import {orderBookRoute} from '@/features/orderBook/routes';
 import {heatmapRoute} from '@/features/heatmap/routes';
 import {betaRoute} from '@/features/beta/routes';
+// import {postRoute} from '@/features/posts/views/post';
+// import {postsRoute} from '@/features/posts/views/posts';
+// import {postsIndexRoute} from '@/features/posts/views/postsIndex';
 
 const routeTree = homeRoute.addChildren([
     indexRoute,
     orderBookRoute,
     heatmapRoute,
-    postsRoute.addChildren([postsIndexRoute, postRoute]),
     betaRoute,
+    // postsRoute.addChildren([postsIndexRoute, postRoute]),
 ]);
 
 const router = new ReactRouter({
