672ec1ab931ae52bf9505999609bfb1fe94f3c3c
Andrej Prskalo <aprskalo00@gmail.com>
2023-09-18 13:00:34 +0200

    removed useMemo from chart options

--- src/features/stats/components/BetaHeatmap.tsx
+++ src/features/stats/components/BetaHeatmap.tsx
@@ -6,61 +6,58 @@ import ChartContainer from './ChartContainer';
 import {useBetaHeatMapData} from '../api';
 
 const BetaHeatmap = () => {
-    const betaHeatmap = useBetaHeatMapData('3m');
+    const betaHeatmap = useBetaHeatMapData('6m');
 
-    const option = useMemo(
-        () => ({
-            grid: {top: 20, right: 60, bottom: 30, left: 50},
-            tooltip: {
-                position: 'top',
+    const option = {
+        grid: {top: 20, right: 60, bottom: 30, left: 50},
+        tooltip: {
+            position: 'top',
+        },
+        xAxis: {
+            type: 'category',
+            data: betaHeatmap?.data?.xAxis,
+            splitArea: {
+                show: true,
             },
-            xAxis: {
-                type: 'category',
-                data: betaHeatmap?.data?.xAxis,
-                splitArea: {
+        },
+        yAxis: {
+            type: 'category',
+            data: betaHeatmap?.data?.yAxis,
+            splitArea: {
+                show: true,
+            },
+        },
+        visualMap: {
+            min: -1,
+            max: 1,
+            calculable: true,
+            orient: 'vertical',
+            right: '2',
+            bottom: '25%',
+            textStyle: {
+                color: '#fff',
+            },
+        },
+        series: [
+            {
+                type: 'heatmap',
+                data: betaHeatmap?.data?.data,
+                label: {
                     show: true,
                 },
-            },
-            yAxis: {
-                type: 'category',
-                data: betaHeatmap?.data?.yAxis,
-                splitArea: {
-                    show: true,
-                },
-            },
-            visualMap: {
-                min: -1,
-                max: 1,
-                calculable: true,
-                orient: 'vertical',
-                right: '2',
-                bottom: '25%',
-                textStyle: {
-                    color: '#fff',
-                },
-            },
-            series: [
-                {
-                    type: 'heatmap',
-                    data: betaHeatmap?.data?.data,
-                    label: {
-                        show: true,
-                    },
-                    emphasis: {
-                        itemStyle: {
-                            shadowBlur: 10,
-                            shadowColor: 'rgba(0, 0, 0, 0.5)',
-                        },
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)',
                     },
                 },
-            ],
-        }),
-        [betaHeatmap.isFetched],
-    );
+            },
+        ],
+    };
 
     return (
         <ChartContainer
-            header={<h3>Pearson correlation</h3>}
+            header={<h3>Pearson correlation: 6m</h3>}
             body={<ReactEcharts option={option} style={{width: '100%', height: '92%'}}></ReactEcharts>}
         />
     );
--- src/features/stats/components/PriceChangePerDayOfWeek.tsx
+++ src/features/stats/components/PriceChangePerDayOfWeek.tsx
@@ -13,38 +13,35 @@ const PriceChangePerDayOfWeek = (props: {tf: string}) => {
 
     const priceChangePerDayOfWeekData = usePriceChangePerDayOfWeek(tf);
 
-    const option = useMemo(
-        () => ({
-            grid: {top: 20, right: 20, bottom: 30, left: 45},
-            tooltip: {
-                trigger: 'axis',
-                axisPointer: {
-                    type: 'shadow',
-                },
-                formatter: function (params: any) {
-                    const colorDot = `<span style="display:inline-block; width: 8px; height: 8px; border-radius: 50%; background-color:${params[0].color}; margin-right: 4px;"></span>`;
-                    return colorDot + params[0].name + ': ' + params[0].value + '%';
-                },
+    const option = {
+        grid: {top: 20, right: 20, bottom: 30, left: 45},
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow',
             },
-            xAxis: {
-                type: 'category',
-                data: priceChangePerDayOfWeekData?.data?.xAxis,
+            formatter: function (params: any) {
+                const colorDot = `<span style="display:inline-block; width: 8px; height: 8px; border-radius: 50%; background-color:${params[0].color}; margin-right: 4px;"></span>`;
+                return colorDot + params[0].name + ': ' + params[0].value + '%';
             },
-            yAxis: {
-                type: 'value',
-                axisLabel: {
-                    formatter: '{value}%',
-                },
+        },
+        xAxis: {
+            type: 'category',
+            data: priceChangePerDayOfWeekData?.data?.xAxis,
+        },
+        yAxis: {
+            type: 'value',
+            axisLabel: {
+                formatter: '{value}%',
             },
-            series: [
-                {
-                    data: priceChangePerDayOfWeekData?.data?.data,
-                    type: 'bar',
-                },
-            ],
-        }),
-        [priceChangePerDayOfWeekData],
-    );
+        },
+        series: [
+            {
+                data: priceChangePerDayOfWeekData?.data?.data,
+                type: 'bar',
+            },
+        ],
+    };
 
     return (
         <ChartContainer
