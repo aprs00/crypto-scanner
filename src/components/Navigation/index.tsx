e0ebde275418910d9f03c5f9b435849834a879b5
Andrej Prskalo <aprskalo00@gmail.com>
2023-10-06 15:33:49 +0200

    changed id for table

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,18 +1,37 @@
 import {memo} from 'react';
 import {Link} from '@tanstack/react-router';
 
-import {navigationLinks} from '@/config/links';
+// import {navigationLinks} from '@/config/links';
 
 const Navigation = () => {
-    const links = navigationLinks.map((link) => (
-        <Link key={link.path} to={link.path} activeProps={{className: 'font-bold'}}>
-            {link.name}
-        </Link>
-    ));
-
     return (
         <div className="flex items-center justify-between py-3 mx-3">
-            <div className="flex gap-3 text-lg">{links}</div>
+            <div className="flex gap-3 text-lg">
+                <Link
+                    to="/"
+                    activeProps={{
+                        className: 'font-bold',
+                    }}
+                >
+                    Stats
+                </Link>
+                <Link
+                    to="/chart"
+                    activeProps={{
+                        className: 'font-bold',
+                    }}
+                >
+                    Chart
+                </Link>
+                <Link
+                    to="/heatmap"
+                    activeProps={{
+                        className: 'font-bold',
+                    }}
+                >
+                    Heatmap
+                </Link>
+            </div>
         </div>
     );
 };
