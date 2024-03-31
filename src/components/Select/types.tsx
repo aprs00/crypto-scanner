7007775c3714db40c7ce5f9e26a2a58d0640c9e5
aprs00 <ploutos@DESKTOP-LQ6OCIF>
2024-03-31 20:44:29 +0200

    remove unnecessary router files

--- src/components/Select/types.tsx
+++
@@ -1,7 +0,0 @@
-import {Dispatch, SetStateAction} from 'react';
-
-export type SelectPropsType = {
-    options: {value: string; label: string}[];
-    value: string;
-    onChange: Dispatch<SetStateAction<string>>;
-};
