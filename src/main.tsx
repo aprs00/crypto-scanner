91c17f6ce6cce9f58ef3190ee784479db9840284
aprs00 <ploutos@DESKTOP-LQ6OCIF>
2024-06-22 23:12:00 +0200

    Add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import './index.css';
 import {QueryClientProvider} from '@tanstack/react-query';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 import {createRouter, RouterProvider} from '@tanstack/react-router';
-import {inject} from '@vercel/analytics';
+import {Analytics} from '@vercel/analytics/react';
 import {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -24,11 +24,10 @@ const rootElement = document.getElementById('root');
 if (!rootElement?.innerHTML) {
     const root = ReactDOM.createRoot(rootElement as HTMLElement);
 
-    inject();
-
     root.render(
         <StrictMode>
             <QueryClientProvider client={queryClient}>
+                <Analytics />
                 <RouterProvider router={router} />
                 <ReactQueryDevtools />
             </QueryClientProvider>
