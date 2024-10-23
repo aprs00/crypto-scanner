e3e74c7a12ff7db5fab901c556c6cdbe295aa852
aprs00 <ploutos@DESKTOP-LQ6OCIF>
2024-10-23 20:33:08 +0200

    Fix text overflow

--- src/components/Layouts/CSNavigation/CSNavigation.tsx
+++ src/components/Layouts/CSNavigation/CSNavigation.tsx
@@ -5,7 +5,7 @@ import {headerLinks} from '@/config/links';
 const CSNavigation = () => {
     return (
         <div className="flex items-center justify-between p-3">
-            <div className="flex gap-3 text-lg">
+            <div className="flex gap-3 text-lg overflow-auto text-nowrap">
                 {headerLinks.map((link) => (
                     <Link className="[&.active]:font-bold" key={link.path} to={link.path}>
                         {link.label}
