e6687aeac08dcd868fb339e25033c71b5efb6209
aprs00 <ploutos@DESKTOP-LQ6OCIF>
2024-10-24 21:12:52 +0200

    Migrate to axios

--- src/routes/~treemap/api.tsx
+++ src/routes/~treemap/api.tsx
@@ -1,14 +1,13 @@
 import {useQuery} from '@tanstack/react-query';
 
-import env from '@/config/env';
-import api from '@/lib/ky';
+import api from '@/lib/api';
 
 import type {HeatmapResponseType} from './types';
 
 const fetchHeatmapData = async () => {
-    const url = new URL('api/v3/global/tree_map_chart_data', env.coingeckoAPI);
+    const url = 'api/v3/global/tree_map_chart_data';
 
-    return (await api.get(url).json()) as HeatmapResponseType[];
+    return await api.get<HeatmapResponseType>(url);
 };
 
 const useHeatmapData = () => {
